34d65344703494077c7af4265353d929c926db2a
shinichikebs <hjelbert37@gmail.com>
2024-09-05 17:37:44 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    theme: {
+      extend: {
+        dropShadow: {
+          '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '4xl': [
+              '0 35px 35px rgba(0, 0, 0, 0.25)',
+              '0 45px 65px rgba(0, 0, 0, 0.15)'
+          ]
+        }
+        
+      }
+      
+    }
+  }
+  module.exports = {
+    darkMode: 'class',
+  };
